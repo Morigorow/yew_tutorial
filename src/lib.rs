8478b36458ffc903b80bd5d513c08e139909e780
morigorow <92914853+Morigorow@users.noreply.github.com>
2021-12-10 21:24:54 +0900

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+struct TodoApp {
+    link: ComponentLink<Self>,
+    todos: Option<Vec<Todo>>,
+    fetch_task: Option<FetchTask>,
+}
+
+#[derive(Deserialize, Clone, PartialEq, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Todo {
+    pub user_id: u64,
+    pub id: u64,
+    pub title: String,
+    pub completed: bool,
+}
